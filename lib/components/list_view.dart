1ff6837b83bdcb3168743df85711295baff1a8fc
masha yanson <mashayanson@gmail.com>
2022-04-15 21:19:38 +0300

    Merge remote-tracking branch 'origin/var2' into var2 t

--- lib/components/list_view.dart
+++ lib/components/list_view.dart
@@ -18,43 +18,6 @@ class Mylist extends StatelessWidget {
             subtitle: Text(" Artist name - Unknown Album - Unknown Track"),
           );
       } ,
-      // children: const <Widget>[
-      //   ListTile(
-      //     leading: CircleAvatar( foregroundImage: AssetImage("assets/vinilovye-plastinki-1.jpg"),),
-      //     title: Text("20"),
-      //     subtitle: Text(" Artist name - Unknown Album - Unknown Track"),
-      //   ),
-      //
-      // ],
     );
   }
 }
-//
-// class MylistItem extends StatelessWidget {
-//   const MylistItem({Key? key}) : super(key: key);
-//   final int number;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-// }
-//
-//
-// class MySepList extends StatelessWidget {
-//   MySepList({Key? key}) : super(key: key);
-//
-//   var list = new List<int>.generate(10, (i) => i+1);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.separated(
-//         padding: const EdgeInsets.all(8),
-//         itemCount: list.length,
-//         itemBuilder:(BuildContext, int index) {
-//           return MylistItem(number: index)
-//         },
-//         separatorBuilder: (BuildContext context, int index) => const Divider(thickness: 3,) ,
-//     );
-//   }
-// }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:widgets/screens/home_screen.dart';
+import 'package:widgets/screens/stream_page.dart';
+
+import '../themes/theme_screen.dart';
 
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: MyTheme(),
+      title: "Named Routes",
+      initialRoute: '/',
+      routes: {
+        '/': (context) =>  LoginScreen(),
+         '/users': (context) => StreamPage(),
+      },
+
+    );
+  }
+}
+
+
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const borderStyle = OutlineInputBorder(
@@ -16,73 +36,88 @@ class MyApp extends StatelessWidget {
         fontWeight: FontWeight.bold,
         color: Color(0xFF0079D0)
     );
+    String phoneToCompare = '+712345';
+    String passToCompare = "pass";
+    String phone = '';
+    String pass = '';
+    void _handleLogin () {
+      if(phone == phoneToCompare && pass == passToCompare) {
+        Navigator.pushNamed(context, "/users");
 
-    return MaterialApp(
-      title: "Named Routes",
-      home: Scaffold(
-          body: Container(
-            width: double.infinity,
-            child: SingleChildScrollView(
-              child: Column(children: [
-                SizedBox(height: 110,),
-                SizedBox(width: 330, height: 84, child: Image(image: AssetImage('assets/440px-Soundcloud_logo.svg.png')),),
-                SizedBox (height: 30,),
-                const SizedBox(width: 260,
-                  child:Text('Insert your phone number',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6), ),
-                  ),
-                ),
-                SizedBox (height: 30,),
-                const SizedBox(width: 264,
-                  child: TextField(
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration (
-                      filled: true,
-                      fillColor: Color(0xFFeceff1),
-                      enabledBorder: borderStyle,
-                      focusedBorder: borderStyle,
-                      labelText: 'Phone number',
-                    ),
-                  ),
-                ),
-                SizedBox (height: 30,),
-                const SizedBox(width: 264,
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration (
-                      filled: true,
-                      fillColor: Color(0xFFeceff1),
-                      enabledBorder: borderStyle,
-                      focusedBorder: borderStyle,
-                      labelText: 'Password',
-                    ),
-                  ),
-                ),
-                SizedBox (height: 30,),
-                SizedBox(width: 154, height: 42, child:
-                ElevatedButton(onPressed: () {
+      }
+    }
 
-                },
-                  child: Text('Log In'),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(36.0),
-                    ),
-                  ),
-                )
+
+    return Scaffold(
+        body: Container(
+          width: double.infinity,
+          child: SingleChildScrollView(
+            child: Column(children: [
+              SizedBox(height: 110,),
+              SizedBox(width: 330, height: 84, child: Image(image: AssetImage('assets/440px-Soundcloud_logo.svg.png')),),
+              SizedBox (height: 30,),
+              const SizedBox(width: 260,
+                child:Text('Insert your phone number',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6), ),
                 ),
-                SizedBox (height: 60,),
-                InkWell(child: Text('Register', style: linkTextStyle,
-                ), onTap: () {}),
-                SizedBox (height: 30,),
-                InkWell(child: Text('Forgot your password?', style: linkTextStyle,),
-                    onTap: () {}),
-              ],),
-            ),
-          )
-      ),
+              ),
+              SizedBox (height: 30,),
+              SizedBox(width: 264,
+                child: TextField(
+                  onChanged: (text) {
+                    phone = text;
+                  },
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration (
+                    filled: true,
+                    fillColor: Color(0xFFeceff1),
+                    enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
+                    labelText: 'Phone number',
+                  ),
+                ),
+              ),
+              SizedBox (height: 30,),
+              SizedBox(width: 264,
+                child: TextField(
+                  onChanged: (text) {
+                  pass = text;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration (
+                    filled: true,
+                    fillColor: Color(0xFFeceff1),
+                    enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
+                    labelText: 'Password',
+
+                  ),
+                ),
+              ),
+              SizedBox (height: 30,),
+              SizedBox(width: 154, height: 42, child:
+              ElevatedButton(onPressed: () {
+                _handleLogin();
+              },
+                child: Text('Log In'),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(36.0),
+                  ),
+                ),
+              )
+              ),
+              SizedBox (height: 60,),
+              InkWell(child: Text('Register', style: linkTextStyle,
+              ), onTap: () {}),
+              SizedBox (height: 30,),
+              InkWell(child: Text('Forgot your password?', style: linkTextStyle,),
+                  onTap: () {}),
+            ],),
+          ),
+        )
     );
-  }
 }
 
+}
